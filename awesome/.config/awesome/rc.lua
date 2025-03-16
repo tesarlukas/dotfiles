3ebf4b4c84b754bf2ddd42a4ac066ab8831b3b06
tesarlukas <lukastesar90@gmail.com>
2025-03-16 17:51:42 +0100

    feat(awesome): disable alt h

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -511,11 +511,11 @@ globalkeys = mytable.join(
 			beautiful.cal.show(7)
 		end
 	end, { description = "show calendar", group = "widgets" }),
-	awful.key({ altkey }, "h", function()
-		if beautiful.fs then
-			beautiful.fs.show(7)
-		end
-	end, { description = "show filesystem", group = "widgets" }),
+	-- awful.key({ altkey }, "h", function()
+	-- 	if beautiful.fs then
+	-- 		beautiful.fs.show(7)
+	-- 	end
+	-- end, { description = "show filesystem", group = "widgets" }),
 	awful.key({ altkey }, "w", function()
 		if beautiful.weather then
 			beautiful.weather.show(7)
